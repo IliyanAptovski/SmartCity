5188044f2d79825317d508e7b3084d9ab083338e
Iliyan Aptovski <120402706+IliyanAptovski@users.noreply.github.com>
2026-04-09 23:13:37 +0300

    feat:added sensor data

--- scripts/sensors_data.py
+++ scripts/sensors_data.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import django
 import random
 from datetime import timedelta
 from django.utils import timezone
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.settings")
 django.setup()
 
@@ -12,10 +14,23 @@ from apps.sensors.models import Location, Sensor, Measurement
 
 def create_locations():
     locations = [
-        ("City Center", "Main St 1"),
-        ("Industrial Zone", "Factory Rd 12"),
-        ("Residential Area", "Green St 45"),
-        ("University Campus", "Campus Ave 3"),
+        ("Sofia Center", "Vitosha Blvd 1"),
+        ("Plovdiv Old Town", "Saborna St 15"),
+        ("Varna Seaside", "Knyaz Boris I Blvd 77"),
+        ("Burgas Center", "Aleksandrovska St 21"),
+        ("Ruse Downtown", "Tsar Osvoboditel Blvd 5"),
+        ("Stara Zagora Center", "Tsar Simeon Veliki Blvd 88"),
+        ("Pleven Center", "Vazrazhdane Sq 2"),
+        ("Sliven Industrial Zone", "Industrialna St 10"),
+        ("Dobrich Residential Area", "25-ti Septemvri Blvd 44"),
+        ("Shumen Center", "Slavyanski Blvd 12"),
+        ("Pernik Center", "Republika Blvd 3"),
+        ("Haskovo Downtown", "San Stefano St 9"),
+        ("Yambol Center", "Georgi Rakovski St 18"),
+        ("Blagoevgrad Campus Area", "Ivan Mihaylov Blvd 66"),
+        ("Veliko Tarnovo Old Town", "Gurko St 30"),
+        ("Gabrovo Center", "Bryanska St 5"),
+        ("Kardzhali Residential Area", "Bulgaria Blvd 14"),
     ]
 
     objs = []
@@ -30,7 +45,7 @@ def create_sensors(locations):
     sensors = []
 
     for loc in locations:
-        for i in range(3):  # 3 sensors per location
+        for i in range(17):  # 17 sensors per location
             sensor = Sensor.objects.create(
                 name=f"{loc.name}-Sensor-{i+1}",
                 location=loc,
@@ -66,8 +81,8 @@ def generate_measurements(sensors):
             # Too LOW weak flow, possible leak, pump failure: < 1.5 bar
             # Too HIGH dangerous, risk of pipe damage: > 6.5 – 7 bar
 
-            flow = random.uniform(5, 100) # in liters
-            pressure = random.uniform(2, 6) # in bars
+            flow = round(random.uniform(5, 100), 2) # in liters
+            pressure = round(random.uniform(2, 6), 2) # in bars
 
             # INJECT ANOMALY (~15% chance)
             if random.random() < 0.15:
@@ -81,23 +96,23 @@ def generate_measurements(sensors):
 
                 if anomaly_type == "leak":
                     # High flow, low pressure
-                    flow = random.uniform(120, 200)
-                    pressure = random.uniform(0.5, 2.0)
+                    flow = round(random.uniform(120, 200), 2)
+                    pressure = round(random.uniform(0.5, 2.0), 2)
 
                 elif anomaly_type == "blockage":
                     # Very low flow, high pressure buildup
-                    flow = random.uniform(0.0, 2.0)
-                    pressure = random.uniform(6.5, 8.0)
+                    flow = round(random.uniform(0.0, 2.0), 2)
+                    pressure = round(random.uniform(6.5, 8.0), 2)
 
                 elif anomaly_type == "pump_failure":
                     # Low pressure + unstable/low flow
-                    flow = random.uniform(0, 2.0)
-                    pressure = random.uniform(0.5, 1.5)
+                    flow = round(random.uniform(0, 2.0), 2)
+                    pressure = round(random.uniform(0.5, 1.5), 2)
 
                 elif anomaly_type == "overpressure":
                     # Dangerous pressure spike
-                    flow = random.uniform(80, 100)
-                    pressure = random.uniform(7.0, 10.0)
+                    flow = round(random.uniform(80, 100), 2)
+                    pressure = round(random.uniform(7.0, 10.0), 2)
 
             Measurement.objects.create(
                 sensor=sensor,
